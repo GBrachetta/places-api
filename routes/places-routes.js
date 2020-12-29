22b5126cb4b45e959192cfaa3d27be4944800073
Guillermo Brachetta <brachetta@me.com>
2020-12-29 12:18:18 +0100

    Handle errors in middleware

--- routes/places-routes.js
+++ routes/places-routes.js
@@ -2,9 +2,47 @@ const express = require('express');
 
 const router = express.Router();
 
-router.get('/', (req, res, next) => {
-  console.log('GET request in Places');
-  res.json({ message: 'It works!' });
+const DUMMY_PLACES = [
+  {
+    id: 'p1',
+    title: 'Empire State Building',
+    description: 'One of the most famous skyscrapers in the world!',
+    address: '20 W 34th St, New York, NY 10001, United States',
+    location: {
+      lat: 40.7484405,
+      lng: -73.9878531,
+    },
+    creator: 'u1',
+  },
+];
+
+router.get('/:pid', (req, res, next) => {
+  const placeId = req.params.pid;
+  const place = DUMMY_PLACES.find(p => {
+    return p.id === placeId;
+  });
+  if (!place) {
+    const error = new Error('Could not find a place for the provided id.');
+    error.code = 404;
+    throw error;
+  }
+
+  res.json({ place });
+});
+
+router.get('/user/:uid', (req, res, next) => {
+  const userId = req.params.uid;
+
+  const place = DUMMY_PLACES.find(p => {
+    return p.creator === userId;
+  });
+  if (!place) {
+    const error = new Error('Could not find a place for the provided user id.');
+    error.code = 404;
+    return next(error);
+  }
+
+  res.json({ place });
 });
 
 module.exports = router;
