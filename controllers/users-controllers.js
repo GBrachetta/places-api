0214e75f593f61e904f093933cd08ced08de7cea
Guillermo Brachetta <brachetta@me.com>
2020-12-29 22:46:21 +0100

    Add get users and login

--- controllers/users-controllers.js
+++ controllers/users-controllers.js
@@ -3,17 +3,18 @@ const HttpError = require('../models/http-error');
 const { validationResult } = require('express-validator');
 const User = require('../models/user');
 
-const DUMMY_USERS = [
-  {
-    id: 'u1',
-    name: 'Guillermo Brachetta',
-    email: 'brachetta@me.com',
-    password: 'password',
-  },
-];
-
-const getUsers = (req, res, next) => {
-  res.json({ users: DUMMY_USERS });
+const getUsers = async (req, res, next) => {
+  let users;
+  try {
+    users = await User.find({}, '-password');
+  } catch (err) {
+    const error = new HttpError(
+      'Something went wrong. Please try again later',
+      500,
+    );
+    return next(error);
+  }
+  res.json({ users: users.map(user => user.toObject({ getters: true })) });
 };
 
 const signup = async (req, res, next) => {
@@ -64,13 +65,27 @@ const signup = async (req, res, next) => {
   res.status(201).json({ user: createdUser.toObject({ getters: true }) });
 };
 
-const login = (req, res, next) => {
+const login = async (req, res, next) => {
   const { email, password } = req.body;
 
-  const identifiedUser = DUMMY_USERS.find(u => u.email === email);
+  let existingUser;
 
-  if (!identifiedUser || identifiedUser.password !== password) {
-    throw new HttpError('Could not identify. Credentials seem wrong', 401);
+  try {
+    existingUser = await User.findOne({ email: email });
+  } catch (err) {
+    const error = new HttpError(
+      'Logging in failed. Please try again later',
+      500,
+    );
+    return next(error);
+  }
+
+  if (!existingUser || existingUser.password !== password) {
+    const error = new HttpError(
+      'Invalid credentials. Could not log you in.',
+      401,
+    );
+    return next(error);
   }
 
   res.json({ message: 'Logged in!' });
